b9062a34b73bdb51f39da6a8d7126cabe85cefd5
Gabriel-227 <135710459+Gabriel-227@users.noreply.github.com>
2024-05-29 19:23:38 -0300

    Add files via upload

---
+++ Java-uner/practica4.js
@@ -0,0 +1,5 @@
+let numero = 10;
+//Se agrega una variable con valor 10//
+console.log("El numero es: ", numero);
+//llamo la variable numero, la cual aloja en numero 10//
+/*Script importado me dice el numero que tengo almacenado en mi variable numero*/
---
+++ Java-uner/practica5.js
@@ -0,0 +1,3 @@
+let nombre = "Gabriel"
+console.log("Mi nombre es:", nombre)
+//Clase muy buena muy practica y clara en las explicaciones // 
